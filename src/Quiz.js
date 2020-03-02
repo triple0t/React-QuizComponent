c9b3b655210222a77bb8b45f1ad248eb7b378da1
oluwaseun Olorunsola <triple0t1@gmail.com>
2020-03-02 17:13:28 +0100

    feat: completed module4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
@@ -10,8 +11,12 @@ class Quiz extends Component {
     }
   }
   render() {
+    const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
     return <div> 
+      {isQuizEnd ? 
+      <QuizEnd /> :
       <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+      }
     </div>
   }
 }
